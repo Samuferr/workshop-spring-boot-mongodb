de634510a40eeb1a4592121f7215118549ed03e7
Samuferr <samuferr@gmail.com>
2021-06-02 13:12:55 -0300

    Insert User with POST

--- src/main/java/com/samuferr/workshopmongo/services/UserService.java
+++ src/main/java/com/samuferr/workshopmongo/services/UserService.java
@@ -7,21 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.samuferr.workshopmongo.domain.User;
+import com.samuferr.workshopmongo.dto.UserDTO;
 import com.samuferr.workshopmongo.repository.UserRepository;
 import com.samuferr.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 	
-	public User findById(String id) { 
-		  Optional<User> obj = repo.findById(id); 
-		  return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
-		}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
