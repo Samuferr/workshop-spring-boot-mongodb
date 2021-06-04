6ae3a1093b313d6bbde8da0ae75556c9b4526141
Samuferr <samuferr@gmail.com>
2021-06-04 10:46:41 -0300

    Endpoint to return a user's posts

--- src/main/java/com/samuferr/workshopmongo/resources/UserResource.java
+++ src/main/java/com/samuferr/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.samuferr.workshopmongo.domain.Post;
 import com.samuferr.workshopmongo.domain.User;
 import com.samuferr.workshopmongo.dto.UserDTO;
 import com.samuferr.workshopmongo.services.UserService;
@@ -58,6 +59,14 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
+	
 	
 	
 	
